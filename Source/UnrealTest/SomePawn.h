4502c28a0a1eec10022a0c4f962e573d455faded
Velhelm <voodoosoft2012@gmail.com>
2015-11-12 10:33:49 -0600

    Add SomePawn
    I added a test pawn to show how this thing works.

---
+++ Source/UnrealTest/SomePawn.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Pawn.h"
+#include "SomePawn.generated.h"
+
+UCLASS()
+class UNREALTEST_API ASomePawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ASomePawn();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
+
+	
+	
+};
